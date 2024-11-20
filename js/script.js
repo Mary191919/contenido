e0502fe531210db52ccaeee2097bb3d30ed3214c
cielo <U22229709@utp.edu.pe>
2024-11-19 19:15:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let audioContext;
   let analyser;
   let microphone;
-  let audio = new Audio('./audio/happy.mp3');
+  let audio = new Audio('/audio/happy.mp3');
 
 
   function updateCandleCount() {
